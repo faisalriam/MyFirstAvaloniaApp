9d1c2e39063ec9f1ebfa20562d5062e7555d9a80
Faisal Ahmed Riam <ce23o36@mbstu.ac.bd>
2025-04-21 16:46:58 +0600

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,41 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using System;
+
+namespace MyFirstAvaloniaApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnAddClicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int number1 = int.Parse(Input1.Text);
+                int number2 = int.Parse(Input2.Text);
+                int sum = number1 + number2;
+
+                if (radiobutton.IsChecked == true)
+                {
+                    Result.Text = sum.ToString();
+                }
+                else
+                {
+                    var messageBox = MessageBoxManager
+                        .GetMessageBoxStandard("Warning", "Please check the redio button", ButtonEnum.Ok);
+                    await messageBox.ShowAsync();
+                }
+            }
+            catch (FormatException)
+            {
+                Result.Text = "Please enter valid numbers.";
+            }
+        }
+    }
+}
